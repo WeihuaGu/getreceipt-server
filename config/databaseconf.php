485b9e41e3d07fae2f7f2b60d9712db9fbc68069
顾玮华 <weihuagu_work@163.com>
2018-08-20 10:28:47 +0800

    Create databaseconf.php

---
+++ config/databaseconf.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(E_ALL^E_NOTICE);
+
+if($_ENV['MYSQL_DBNAME']!=null){
+ 
+return 
+[
+        'database_type' => 'mysql',
+        'database_name' => $_ENV['MYSQL_DBNAME'],
+        'server' => $_ENV['MYSQL_HOST'],
+        'username' => $_ENV['MYSQL_USERNAME'],
+        'password' =>$_ENV['MYSQL_PASSWORD'],
+        'port' => $_ENV['MYSQL_PORT'],
+        'charset' => 'utf8'
+    ];
+}else
+
+return 
+[
+        'database_type' => 'mysql',
+        'database_name' => 'zhiyu',
+        'server' => 'localhost',
+        'username' => 'root',
+        'password' => '1992',
+        'charset' => 'utf8'
+    ];
