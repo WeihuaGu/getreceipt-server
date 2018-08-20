2ff00a5c21fbd35fc61655a7ce6fed65e5d55c5e
顾玮华 <weihuagu_work@163.com>
2018-08-20 11:11:36 +0800

    Update databaseconf.php

--- config/databaseconf.php
+++ config/databaseconf.php
@@ -1,12 +1,12 @@
 <?php
 error_reporting(E_ALL^E_NOTICE);
 
-if($_ENV['MYSQL_DBNAME']!=null){
+if($_ENV['MYSQL_DBNAME_GETRECEIPT']!=null){
  
 return 
 [
         'database_type' => 'mysql',
-        'database_name' => $_ENV['MYSQL_DBNAME'],
+        'database_name' => $_ENV['MYSQL_DBNAME_GETRECEIPT'],
         'server' => $_ENV['MYSQL_HOST'],
         'username' => $_ENV['MYSQL_USERNAME'],
         'password' =>$_ENV['MYSQL_PASSWORD'],
