b590c0e64cea6178736a19f586af77b1d2929079
顾玮华 <weihuagu_work@163.com>
2018-08-23 23:47:46 +0800

    Update routegetmoney.php

--- route/routegetmoney.php
+++ route/routegetmoney.php
@@ -1,6 +1,12 @@
 <?php 
-Flight::route('POST /getmoney/', function(){
-$body = Flight::request()->getBody();
-Flight::json(array('code'=>Flight::get('httpcode')['get_success'],'desc'=>$body));
+require_once 'common.php';
+Flight::route('POST /getedmoney/', function(){
+$bodyjson = getBodyJson();
+$recordarray=json2Array($bodyjson);
+$flaginsert=new Bill()->recordReceipts($recordarray);
+if($flaginsert)
+Flight::json(array('code'=>Flight::get('httpcode')['created'],'desc'=>'created'));
+else
+Flight::json(array('code'=>Flight::get('httpcode')['conflict'],'desc'=>'conflict'));
 });
 
