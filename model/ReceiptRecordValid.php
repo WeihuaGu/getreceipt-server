caf03ee12fac08362af19130b06da6dca5ac01be
顾玮华 <weihuagu_work@163.com>
2018-08-24 23:37:00 +0800

    Update ReceiptRecordValid.php

--- model/ReceiptRecordValid.php
+++ model/ReceiptRecordValid.php
@@ -1,11 +1,16 @@
 <?php 
 namespace model;
-class  ReceiptRecordValid implements IValidAble{
+class  ReceiptRecordValid implements IValidAble,IMiddleware{
 
 public function verify($data){
 return true;
 }
+  
+public function nextData($data){
+return $data
+}
 
-
-
+public function transJsonname2TableColumname($data){
+return this->nextData($data);
+}
 }
