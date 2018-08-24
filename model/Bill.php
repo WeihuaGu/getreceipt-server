dec011545ba5c3513313e5d4f27c98b0d760cfd0
顾玮华 <weihuagu_work@163.com>
2018-08-24 23:41:50 +0800

    Update Bill.php

--- model/Bill.php
+++ model/Bill.php
@@ -8,13 +8,14 @@ public function __construct(){
 
 public recordReceipts($recored){
   if(validRecord($recored)){
+  $recored=new ReceiptRecordValid->transJsonArray2TableColumArray($recored);
   $this->database->insert($this->tablename,$recored);
   return true;
   }else
       return false;
 }
 public validRecord($recored){
-  return new ReceiptRecordValid.verify($recored);
+  return new ReceiptRecordValid->verify($recored);
 }
 
 }
