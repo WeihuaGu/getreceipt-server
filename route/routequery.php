147469259bed3133a4889610da10ec2c1a8e62de
顾玮华 <weihuagu_work@163.com>
2018-08-31 23:21:49 +0800

    Update routequery.php

--- route/routequery.php
+++ route/routequery.php
@@ -1,6 +1,9 @@
 <?php 
 require_once 'common.php';
-
+use model\BillQuery;
 Flight::route('Get /querybill/now', function(){
+$billquery=new BillQuery();
+$nowbill=$billquery->queryNowBill();
+print_r($nowbill);
 
 });
